028e0f04a4fbcae8cab66265568cb2b06c75fe2e
sarah-nolan <snolan202@gmail.com>
2016-12-18 17:36:05 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $('#citypix-form').submit(function(event){
+    var city = $('#city-type').val();
+
+    if(city === "New York" || city === "new york" || city ===  "NYC" || city === "nyc" || city === "New York City" || city === "new york city")  {
+      $("body").removeClass().addClass("nyc");
+      $(".error-message").addClass("hidden");
+    } else if (city === "Austin" || city ===  "austin" || city === "ATX" || city === "atx") {
+      $("body").removeClass().addClass("austin");
+      $(".error-message").addClass("hidden");
+    } else if (city === "Los Angeles" || city ===  "LA" || city === "L.A." || city === "la" || city === "l.a." || city === "Hollywood" || city === "hollywood" || city === "LAX" || city === "lax") {
+      $("body").removeClass().addClass("la");
+      $(".error-message").addClass("hidden");
+    } else if (city === "San Francisco" || city === "san francisco" || city === "SF" || city === "sf" || city === "Bay Area" || city === "bay area") {
+      $("body").removeClass().addClass("sf");
+    } else if (city === city === "Sydney" || city === "sydney" || city === "SYD" || city === "syd") {
+      $("body").removeClass().addClass("sydney");
+      $(".error-message").addClass("hidden");
+    } else {
+      $("body").removeClass();
+      $(".error-message").removeClass("hidden");
+    }
+    event.preventDefault();
+  });
+
+});
